2e7b96c8249202c7273b076352a95f4b1c0a8dcb
maubg-debug <maubg@tutanota.com>
2021-08-12 18:58:22 +0200

    nearly completed CLI

--- app/form.js
+++ app/form.js
@@ -1,53 +1,103 @@
-var {openForm} = require('ink-form');
+var { openForm } = require("ink-form");
 
-exports.form = async (default_info) => {
+exports.form = async (default_info, options) => {
   const licenses = [
-    { name: 'Apache 2.0', value: 'Apache-2.0' },
-    { name: 'MIT', value: 'MIT' },
-    { name: 'Mozilla Public License 2.0', value: 'MPL-2.0' },
-    { name: 'BSD 2-Clause (FreeBSD) License', value: 'BSD-2-Clause-FreeBSD' },
-    { name: 'BSD 3-Clause (NewBSD) License', value: 'BSD-3-Clause' },
-    { name: 'Internet Systems Consortium (ISC) License', value: 'ISC' },
-    { name: 'GNU AGPL 3.0', value: 'AGPL-3.0' },
-    { name: 'GNU GPL 3.0', value: 'GPL-3.0' },
-    { name: 'GNU LGPL 3.0', value: 'LGPL-3.0' },
-    { name: 'Unlicense', value: 'unlicense' },
-    { name: 'No License (Copyrighted)', value: 'UNLICENSED' }
+    { label: "Apache 2.0", value: "Apache-2.0" },
+    { label: "MIT", value: "MIT" },
+    { label: "Mozilla Public License 2.0", value: "MPL-2.0" },
+    { label: "BSD 2-Clause (FreeBSD) License", value: "BSD-2-Clause-FreeBSD" },
+    { label: "BSD 3-Clause (NewBSD) License", value: "BSD-3-Clause" },
+    { label: "Internet Systems Consortium (ISC) License", value: "ISC" },
+    { label: "GNU AGPL 3.0", value: "AGPL-3.0" },
+    { label: "GNU GPL 3.0", value: "GPL-3.0" },
+    { label: "GNU LGPL 3.0", value: "LGPL-3.0" },
+    { label: "Unlicense", value: "unlicense" },
+    { label: "No License (Copyrighted)", value: "UNLICENSED" },
   ];
 
   const { user } = default_info;
 
-  const mardown = [
-    {name: 'yes', value: true},
-    {name: 'no', value: false}
-  ]
-  
   const form = {
     form: {
-      title: 'Form title',
+      title: `Create ${options.output}`,
       sections: [
         {
-          title: 'basic info',
+          title: "basic info",
+          description: '\nBasic information for your license. In the exetended information tab you can see more options.\n',
           fields: [
-            { type: 'string', name: 'name', label: 'What\'s your name?', required: user.name ? false : true },
-            { type: 'string', name: 'email', label: 'Your email (optional)', initalValue: user.email || 'null' },
-            { type: 'string', name: 'website', label: 'Your website (optional)', initalValue: 'null' },
-            { type: 'select', name: 'license', label: 'Select license', options: licenses, required: true },
+            {
+              type: "string",
+              name: "name",
+              label: "What's your name?"
+            },
+            {
+              type: "string",
+              name: "email",
+              label: "Your email (optional)",
+              initialValue: user.email || "null",
+            },
+            {
+              type: "string",
+              name: "website",
+              label: "Your website (optional)",
+              // initialValue: "null",
+            },
+            {
+              type: "select",
+              name: "license",
+              label: "Select license",
+              options: licenses,
+              description: 'select between one of these available licenses for your project.',
+              required: true,
+            },
           ],
         },
         {
-          title: 'Extra information',
+          title: "Extended information",
+          description: '\nThis is the section in where you can select extended options.\n',
           fields: [
-            { type: 'boolean', name: 'markdown', label: 'Markdown?' },
+            {
+              type: "boolean",
+              name: "markdown",
+              label: "Markdown? (optional)",
+              description: 'If this options is set to True, \nthe license file will be added as a markdown file.'
+            },
+            {
+              type: "string",
+              name: 'output',
+              description: 'This option will add the license with the output you whant.',
+              label: 'Output name for the license (optional)'
+            },
+            {
+              type: "integer",
+              name: 'year',
+              label: 'License year (optional)',
+              initialValue: options.year,
+              max: new Date().getFullYear(),
+              min: 0
+            }
           ],
         },
+        {
+          title: 'License Section',
+          description: [
+            '\nWith this software, you can create a license.\n\nMade with ink-form. Ink-form is a Node library for displaying a user-friendly form in a terminal window. It can be used in two ways, either by using the React Ink component Form exported by the package, or by using the imperative API openForm(options).\n\ncheck out licenser\'s repository here: https://github.com/maubg-debug/generator-licenser.',
+          ],
+          fields: [],
+        }
       ],
     },
   };
 
-  if (user.name)
-    form.form.sections[0].fields[0].initalValue = user.name
+  if (user.name) {
+    form.form.sections[0].fields[0].initialValue = user.name;
+  } else form.form.sections[0].fields[0].required = true;
+
+  if (options.license) {
+    form.form.sections[0].fields[3].required = false;
+    form.form.sections[0].fields[3].initialValue = options.license;
+  }
 
   const result = await openForm(form);
-  return result
-}
+  return result;
+};
--- app/index.js
+++ app/index.js
@@ -8,21 +8,78 @@ module.exports = class extends Generator {
     // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
 
-    this.option('overwrite'); // This method adds support for a `--overwrite` flag
+    this.option('name', {
+      type: String,
+      desc: 'Name of the license owner',
+      required: false
+    });
+
+    this.option('email', {
+      type: String,
+      desc: 'Email of the license owner',
+      required: false
+    });
+
+    this.option('website', {
+      type: String,
+      desc: 'Website of the license owner',
+      required: false
+    });
+
+    this.option('year', {
+      type: String,
+      desc: 'Year(s) to include on the license',
+      required: false,
+      defaults: new Date().getFullYear()
+    });
+
+    this.option('license', {
+      type: String,
+      desc: 'Select a license, so no license prompt will happen, in case you want to handle it outside of this generator',
+      required: false
+    });
+
+    this.option('output', {
+      type: String,
+      desc: 'Set the output file for the generated license',
+      required: false,
+      defaults: 'LICENSE'
+    });
   }
 
   initializing() {
+    clearTerminal();
     this.gitc = {
       user: {
-        name: this.user.git.name(),
-        email: this.user.git.email()
+        name: this.options.name || this.user.git.name(),
+        email: this.options.email || this.user.git.email()
       }
     };
   }
 
   async prompt() {
-    this.data = form(this.gitc)
+    this.data = await form(this.gitc, this.options);
+    clearTerminal();
   }
 
-  
+  writeTemplate() {
+    const filename = this.data.license + '.txt';
+
+    let author = this.data.name.trim();
+    if (this.data.email)
+      author += ' <' + this.data.email.trim() + '>';
+
+    if (this.data.website)
+      author += ' (' + this.data.website.trim() + ')';
+
+    this.fs.copyTpl(
+      this.templatePath(filename),
+      this.destinationPath(`${this.data.output || this.options.output}${this.data.markdown ? '.md' : ''}`),
+      {
+        year: this.data.year,
+        author: author
+      }
+    );
+  }
+
 };
